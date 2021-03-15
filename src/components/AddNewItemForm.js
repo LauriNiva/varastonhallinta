8b70511bad5f3b9818c54d93fbaa66014ff746bc
Lauri Niva <niva.lauri@gmail.com>
2021-03-15 15:47:02 +0200

    Replace div with React.Fragment

--- src/components/AddNewItemForm.js
+++ src/components/AddNewItemForm.js
@@ -1,20 +1,20 @@
+import React from 'react';
 
 const AddNewItemForm = ({ addItem, newItemElguide, handleElguideChange, newItemName, handleNameChange, handleCategoryChange, categories }) => {
 
     const radiobuttons = categories.map(category =>
-        <div key={category.id}>
+        <React.Fragment key={category.id}>
             <input type="radio" name="category" id={category.id} value={category.id} onClick={handleCategoryChange} />
             <label htmlFor={category.id}>{category.name}</label>
-        </div>
+        </React.Fragment>
     )
 
     return (
         <div className="new-item-form">
             <form onSubmit={addItem}>
                 Guidekoodi <input value={newItemElguide} onChange={handleElguideChange} /><br />
-          Tuotekoodi <input value={newItemName} onChange={handleNameChange} /><br />
-          Kategoria
-          {radiobuttons}
+                Tuotekoodi <input value={newItemName} onChange={handleNameChange} /><br />
+                Kategoria {radiobuttons}
                 <br />
                 <button type="submit">Lisää</button>
             </form>
