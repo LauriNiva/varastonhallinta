734d25ec2da3fe6bd260087ac32d7badb4c2ea05
Lauri Niva <niva.lauri@gmail.com>
2021-03-15 08:04:00 +0200

    Basic functionality for loading list of items from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
 
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Items = ({items}) => {
+  return(
+    <div className="items">
+      <ul>
+        {items.map(item => <li key={item.id}>{item.elguide}:{item.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
 
 function App() {
+
+  const [goods, setGoods] = useState([]);
+  const [newItemElguide, setNewItemElguide] = useState("");
+  const [newItemName, setNewItemName] = useState("");
+
+  useEffect(() =>{
+    axios
+    .get("http://localhost:3001/goods")
+    .then(response =>{
+      setGoods(response.data);
+      setNewItemElguide("");
+      setNewItemName("");
+    });
+  }, []);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = {elguide: newItemElguide, name: newItemName};
+    axios
+    .post("http://localhost:3001/goods", newItem)
+    .then(response=>setGoods(goods.concat(response.data)));
+
+  }
+
+  const handleElguideChange = (e) => {
+    setNewItemElguide(e.target.value);
+  }
+
+  const handleNameChange = (e) => {
+    setNewItemName(e.target.value);
+  }
+
+
+  const goodsToShow = goods;
+
+  console.log(goodsToShow);
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <h1>Varastonhallinta</h1>
+
+    <h4>Lisää tuote</h4>
+    <form onSubmit={addItem}>
+      Guidekoodi <input value={newItemElguide} onChange={handleElguideChange}/><br/>
+      Tuotekoodi <input value={newItemName} onChange={handleNameChange}/><br/>
+      <button type="submit">Lisää</button>
+    </form>
+
+
+    <h4>Tuotteet varastossa</h4>
+    <input></input>
+    <Items items={goodsToShow} />
+
+
+
     </div>
   );
 }
