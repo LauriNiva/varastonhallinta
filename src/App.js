34d76f92aae98712b2f76ded01bac53167c5b561
Lauri Niva <niva.lauri@gmail.com>
2021-03-15 08:44:16 +0200

    Delete button for listen items
    Function to delete item with ID. Button for each listed item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Items from './components/Items';
 
@@ -12,24 +12,40 @@ function App() {
   const [newItemName, setNewItemName] = useState("");
   const [itemsFilter, setItemsFilter] = useState("");
 
-  useEffect(() =>{
+  useEffect(() => {
     axios
-    .get("http://localhost:3001/goods")
-    .then(response =>{
-      setGoods(response.data);
-      setNewItemElguide("");
-      setNewItemName("");
-    });
+      .get("http://localhost:3001/goods")
+      .then(response => {
+        setGoods(response.data);
+      });
   }, []);
 
   const addItem = (e) => {
     e.preventDefault();
-    const newItem = {elguide: newItemElguide, name: newItemName};
+    const newItem = { elguide: newItemElguide, name: newItemName };
     axios
-    .post("http://localhost:3001/goods", newItem)
-    .then(response=>setGoods(goods.concat(response.data)));
+      .post("http://localhost:3001/goods", newItem)
+      .then(response => {
+        setGoods(goods.concat(response.data))
+        setNewItemElguide("");
+        setNewItemName("")
+      });
 
   }
+
+  const deleteItem = (id) => {
+
+    const itemUrl = `http://localhost:3001/goods/${id}`;
+
+    axios
+      .delete(itemUrl)
+      .then(response => {
+        console.log("beep");
+        setGoods(goods.filter(item => item.id !== id))
+
+      });
+  }
+
 
   const handleElguideChange = (e) => {
     setNewItemElguide(e.target.value);
@@ -44,28 +60,28 @@ function App() {
   }
 
 
-  const goodsToShow = goods.filter(item => 
+  const goodsToShow = goods.filter(item =>
     item.name.toLowerCase().includes(itemsFilter.toLowerCase())
     || item.elguide.toLowerCase().includes(itemsFilter.toLowerCase())
-    );
+  );
 
-  console.log(goodsToShow);
+  //console.log(goodsToShow);
 
   return (
     <div className="app">
       <h1>Varastonhallinta</h1>
 
-    <h4>Lisää tuote</h4>
-    <form onSubmit={addItem}>
-      Guidekoodi <input value={newItemElguide} onChange={handleElguideChange}/><br/>
-      Tuotekoodi <input value={newItemName} onChange={handleNameChange}/><br/>
-      <button type="submit">Lisää</button>
-    </form>
+      <h4>Lisää tuote</h4>
+      <form onSubmit={addItem}>
+        Guidekoodi <input value={newItemElguide} onChange={handleElguideChange} /><br />
+      Tuotekoodi <input value={newItemName} onChange={handleNameChange} /><br />
+        <button type="submit">Lisää</button>
+      </form>
 
 
-    <h4>Tuotteet varastossa</h4>
-    <input value={itemsFilter} onChange={handleFilterChange} />
-    <Items items={goodsToShow} />
+      <h4>Tuotteet varastossa</h4>
+      <input value={itemsFilter} onChange={handleFilterChange} />
+      <Items items={goodsToShow} deleteItem={deleteItem} />
 
 
 
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,8 +1,8 @@
-const Items = ({items}) => {
+const Items = ({items, deleteItem}) => {
     return(
       <div className="items">
         <ul>
-          {items.map(item => <li key={item.id}>{item.elguide}:{item.name}</li>)}
+          {items.map(item => <li key={item.id}>{item.elguide}:{item.name}<button onClick={()=>deleteItem(item.id)}>-</button></li>)}
         </ul>
       </div>
     )
