f03bde710f98eb0bf635a3e14cea9f40bc00b9ee
Lauri Niva <niva.lauri@gmail.com>
2021-03-15 15:59:55 +0200

    Fix category in item list

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,11 +1,12 @@
 const Items = ({ items, deleteItem, categories }) => {
 
     let itemList;
-
+    console.log(categories);
+    console.log(items);
     if (categories[0]) {
         itemList = items.map(item =>
             <li key={item.id}>
-                {item.elguide}: {item.name} - {categories[item.id].name}
+                {item.elguide}: {item.name} - {categories.find(category => category.id === parseInt(item.category)).name}
                 <button onClick={() => deleteItem(item.id)}>-</button>
             </li>
         );
