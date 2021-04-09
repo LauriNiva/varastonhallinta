806b798a27713e4c3f2f1defc3ef67752340cf34
Lauri Niva <niva.lauri@gmail.com>
2021-04-09 07:47:19 +0300

    Update App
    Add appbar.
    Add filler user selection.
    Get user data from backend, when user is selected.
    Get storages data when user is selected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
-
 import './App.css';
+import 'fontsource-roboto';
+import React, { useState, useEffect } from 'react';
+import userService from './services/users';
 
-function App() {
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
+
+import getStorages from './services/storages';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginLeft: 'auto',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+
+
+const App = () => {
+
+
+  const classes = useStyles();
+
+  const [user, setUser] = useState({ name: 'none' });
+  const [storages, setStorages] = useState([]);
+
+  useEffect(() => {
+    if (user._id){
+    getStorages(user._id)
+      .then(storages => {
+        setStorages(storages);
+        console.log(storages);
+      })}
+  }, [user]);
+
+  const getUser = (e) => {
+    userService(e.target.value)
+      .then(res => {
+        setUser(res);
+      });
+  };
+
+
   return (
     <div className="App">
-      Varasto
+      <AppBar position='sticky'>
+        <ToolBar>
+          <Typography variant='h6'>Varastonhallinta</Typography>
+
+          <FormControl className={classes.formControl}>
+            <InputLabel id='user-select-label'>User</InputLabel>
+            <Select
+              labelId='user-select-label'
+              id='user-select'
+              onChange={getUser} >
+              <MenuItem value={"John"}>John</MenuItem>
+              <MenuItem value={"Jane"}>Jane</MenuItem>
+            </Select>
+          </FormControl>
+
+        </ToolBar>
+
+      </AppBar>
+
+      <Typography>{user.name} selected</Typography>
     </div>
   );
 }
