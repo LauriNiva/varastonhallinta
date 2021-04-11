552e0aab09aa9c5e22e46f1bf9ec3a4cf644f620
Lauri Niva <niva.lauri@gmail.com>
2021-04-11 09:56:20 +0300

    Clean App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
 import 'fontsource-roboto';
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import userService from './services/users';
 import storagesService from './services/storages';
+import itemsService from './services/items';
 
 import Nav from './components/Nav';
 import Storages from './components/Storages'
 
-import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import ToolBar from '@material-ui/core/ToolBar';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import Typography from '@material-ui/core/Typography';
 
 import Paper from '@material-ui/core/Paper';
-import itemsService from './services/items';
-import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
+
+
 
 
 
@@ -44,8 +36,6 @@ const Items = ({ items }) => {
 
 
 const App = () => {
-
-
 
 
   const [user, setUser] = useState({ name: 'none' });
@@ -98,20 +88,18 @@ const App = () => {
 
   return (
     <Router>
-    <div className="App">
+      <div className="App">
 
-      <Nav getUser={ getUser } />
-      <Paper className='body-container'>
-        <Switch>
-          <Route path='/' exact  render={(props) => <Storages {...props} storages={storages} selectedStorage={selectedStorage} setSelectedStorage={setSelectedStorage}
-          handleStockDecreaseClick={handleStockDecreaseClick} handleStockIncreaseClick={handleStockIncreaseClick} />} />
-          <Route path='/hallinta' render={(props) => <Items {...props} items={items} />} />
+        <Nav getUser={getUser} />
+        <Paper className='body-container'>
+          <Switch>
+            <Route path='/' exact render={(props) => <Storages {...props} storages={storages} selectedStorage={selectedStorage} setSelectedStorage={setSelectedStorage}
+              handleStockDecreaseClick={handleStockDecreaseClick} handleStockIncreaseClick={handleStockIncreaseClick} />} />
+            <Route path='/hallinta' render={(props) => <Items {...props} items={items} />} />
           </Switch>
-      </Paper>
+        </Paper>
 
-      
-
-    </div>
+      </div>
     </Router>
   );
 }
