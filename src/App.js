b8115bc6f13ca6b1c4bd0fa98b6571f962569761
Lauri Niva <niva.lauri@gmail.com>
2021-04-11 09:54:43 +0300

    Separate Strogaes to own component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import userService from './services/users';
 import storagesService from './services/storages';
 
 import Nav from './components/Nav';
+import Storages from './components/Storages'
 
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
@@ -17,90 +18,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Typography from '@material-ui/core/Typography';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+
 import Paper from '@material-ui/core/Paper';
 import itemsService from './services/items';
 import { BrowserRouter as Router, Route , Switch } from 'react-router-dom';
 
 
-
-
-
-
-
-const StoragesBar = ({ storages, selectedStorage, setSelectedStorage }) => {
-
-  const handleChange = (e, newTab) => {
-    setSelectedStorage(newTab);
-  };
-
-  return (
-    <div className='storagetabs'>
-      <Tabs value={selectedStorage} onChange={handleChange}>
-        {storages.map((storage, i) => <Tab value={i} key={storage._id} label={storage.name} />)}
-      </Tabs>
-    </div>
-  )
-};
-
-const StorageItemsTable = ({ storage, handleStockDecreaseClick, handleStockIncreaseClick }) => {
-
-
-  if (storage === undefined) return <div></div>;
-
-  const createData = (code, name, category, stock) => {
-    return { code, name, category, stock };
-  };
-
-  const rows = storage.items.map(item => createData(item.itemcode, item.name, item.category, item.stock));
-
-  return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Tuotekoodi</TableCell>
-            <TableCell>Tuote</TableCell>
-            <TableCell align="right">Categoria</TableCell>
-            <TableCell align="right">Määrä</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, i) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.code}
-              </TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell align="right">{row.category}</TableCell>
-              <TableCell align="right">
-                <Button id={`decrease-${i}`} onClick={handleStockDecreaseClick}>-</Button>
-                {row.stock}
-                <Button id={`increase-${i}`} onClick={handleStockIncreaseClick}>+</Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  )
-};
-
-const Storages = ({ storages, selectedStorage, setSelectedStorage, handleStockDecreaseClick, handleStockIncreaseClick }) => {
-
-
-  return (
-    <div>
-      <StoragesBar storages={storages} selectedStorage={selectedStorage} setSelectedStorage={setSelectedStorage} />
-      <StorageItemsTable storage={storages[selectedStorage]} handleStockDecreaseClick={handleStockDecreaseClick} handleStockIncreaseClick={handleStockIncreaseClick} />
-    </div>
-  )
-};
 
 const Items = ({ items }) => {
 
---
+++ src/components/Storages.js
@@ -0,0 +1,83 @@
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+
+
+const StoragesBar = ({ storages, selectedStorage, setSelectedStorage }) => {
+
+  const handleChange = (e, newTab) => {
+    setSelectedStorage(newTab);
+  };
+
+  return (
+    <div className='storagetabs'>
+      <Tabs value={selectedStorage} onChange={handleChange}>
+        {storages.map((storage, i) => <Tab value={i} key={storage._id} label={storage.name} />)}
+      </Tabs>
+    </div>
+  )
+};
+
+const StorageItemsTable = ({ storage, handleStockDecreaseClick, handleStockIncreaseClick }) => {
+
+
+  if (storage === undefined) return <div></div>;
+
+  const createData = (code, name, category, stock) => {
+    return { code, name, category, stock };
+  };
+
+  const rows = storage.items.map(item => createData(item.itemcode, item.name, item.category, item.stock));
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Tuotekoodi</TableCell>
+            <TableCell>Tuote</TableCell>
+            <TableCell align="right">Categoria</TableCell>
+            <TableCell align="right">Määrä</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.code}
+              </TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell align="right">{row.category}</TableCell>
+              <TableCell align="right">
+                <Button id={`decrease-${i}`} onClick={handleStockDecreaseClick}>-</Button>
+                {row.stock}
+                <Button id={`increase-${i}`} onClick={handleStockIncreaseClick}>+</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+};
+
+const Storages = ({ storages, selectedStorage, setSelectedStorage, handleStockDecreaseClick, handleStockIncreaseClick }) => {
+
+
+  return (
+    <div>
+      <StoragesBar storages={storages} selectedStorage={selectedStorage} setSelectedStorage={setSelectedStorage} />
+      <StorageItemsTable storage={storages[selectedStorage]} handleStockDecreaseClick={handleStockDecreaseClick} handleStockIncreaseClick={handleStockIncreaseClick} />
+    </div>
+  )
+};
+
+
+export default Storages;
